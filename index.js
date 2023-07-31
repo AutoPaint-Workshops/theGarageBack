a5c866217963ac82e434a199c8573e08bd6beef1
Welinton Suarez <wesgdeveloper@gmail.com>
2023-07-31 11:38:01 -0500

    Add prisma and prismaClient

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ import http from "http";
 
 import { configuration } from "./app/config.js";
 import { app } from "./app/index.js";
+import { connect } from "./app/database.js";
 const { port } = configuration.server;
+
+// Connect to database
+connect();
 
 const server = http.createServer(app);
 
