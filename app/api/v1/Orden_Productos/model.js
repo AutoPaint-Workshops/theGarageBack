0786bf9524483163bc687a8ce5294b5b05b7e4c6
DianaBedoya <dianabedoya570@gmail.com>
2023-08-21 16:33:50 -0500

    Se inicia la configuracion del crud para Orden_productos

---
+++ app/api/v1/Orden_Productos/model.js
@@ -0,0 +1,7 @@
+export const fields = [
+  "id_cliente",
+  "fecha_orden",
+  "subtotal",
+  "total",
+  "estado",
+];
--- app/api/v1/index.js
+++ app/api/v1/index.js
@@ -2,8 +2,11 @@ import { Router } from "express";
 
 import { router as products } from "./products/routes.js";
 
+import { router as ordenProductos } from "./Orden_Productos/routes.js";
+
 // eslint-disable-next-line new-cap
 export const router = Router();
 
 router.use("/products", products);
+router.use("/orden_productos", ordenProductos);
 // router.use("/users", users);
