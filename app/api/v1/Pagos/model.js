737f12c37b1ae6cb6065568f00f429f66ef47461
DianaBedoya <dianabedoya570@gmail.com>
2023-08-23 10:58:49 -0500

    Se agregan al API las funciones crud para orden_servicios, detalle_orden_servicios y pagos

---
+++ app/api/v1/Pagos/model.js
@@ -0,0 +1,6 @@
+export const fields = [
+  "id",
+  "id_cliente",
+  "id_orden_producto",
+  "id_orden_servicio",
+];
--- app/api/v1/index.js
+++ app/api/v1/index.js
@@ -1,15 +1,18 @@
 import { Router } from "express";
 
-import { router as products } from "./products/routes.js";
+import { router as pagos } from "./Pagos/routes.js";
 
 import { router as ordenProductos } from "./Orden_Productos/routes.js";
 import { router as detalleOrdenProductos } from "./Detalle_Orden_Productos/routes.js";
-
+import { router as ordenServicios } from "./Orden_Servicios/routes.js";
+import { router as detalleOrdenServicios } from "./Detalle_Orden_Servicios/routes.js";
 // eslint-disable-next-line new-cap
 export const router = Router();
 
-router.use("/products", products);
+router.use("/pagos", pagos);
 router.use("/orden_productos", ordenProductos);
+router.use("/orden_servicios", ordenServicios);
 router.use("/detalle_orden_productos", detalleOrdenProductos);
+router.use("/detalle_orden_servicios", detalleOrdenServicios);
 
 // router.use("/users", users);
