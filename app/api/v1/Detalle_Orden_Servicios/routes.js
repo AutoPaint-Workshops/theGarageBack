f672ead66c05eb2aa168953134118e7e92727f8f
DianaBedoya <dianabedoya570@gmail.com>
2023-08-23 11:28:39 -0500

    Se arreglan conflictos con index

---
+++ app/api/v1/Detalle_Orden_Servicios/routes.js
@@ -0,0 +1,25 @@
+import { Router } from "express";
+
+import * as controller from "./controller.js";
+
+// eslint-disable-next-line new-cap
+export const router = Router();
+
+/**
+ * /api/v1/detalle_orden_servicios POST        - CREATE
+ * /api/v1/detalle_orden_servicios GET         - READ ALL
+ * /api/v1/detalle_orden_servicios/:id GET     - READ ONE
+ * /api/v1/detalle_orden_servicios/:id PUT     - UPDATE
+ * /api/v1/detalle_orden_servicios/:id DELETE  - DELETE
+ */
+
+router.route("/").post(controller.create).get(controller.all);
+
+router.param("id", controller.id);
+router.param("id_orden", controller.idOrden);
+router
+  .route("/id")
+  .get(controller.read)
+  .put(controller.update)
+  .patch(controller.update)
+  .delete(controller.remove);
