1088889c07fe90cba907dbbade7d481dd3210f0a
Manuel Ortega Ruiz <manuellortegaruiz@gmail.com>
2023-09-05 09:29:37 -0500

    Add validations with zod on auth

--- app/api/v1/auth/model.js
+++ app/api/v1/auth/model.js
@@ -1,22 +1,69 @@
-import { hash, compare } from 'bcrypt';
+import { z } from 'zod';
 
-export const fields = [
-  'id',
-  'correo',
-  'contrasena',
-  'tipo_usuario',
-  'fecha_creacion',
-  'estatus',
-  'url_foto',
-  'pais',
-  'ciudad',
-  'direccion',
-];
+const UserSchema = z.object({
+  userData: z.object({
+    correo: z.string().trim().max(100).email().toLowerCase(),
+    contrasena: z
+      .string()
+      .min(8)
+      .max(16)
+      .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]+$/),
+    tipo_usuario: z.string(),
+    url_foto: z.nullable(z.string()),
+    pais: z.string(),
+    ciudad: z.string(),
+    direccion: z.string().trim().max(255),
+  }),
+});
 
-export const encryptPassword = (password) => {
-  return hash(password, 10);
+const ClientSchema = z.object({
+  userTypeData: z.object({
+    nombre_completo: z.string().trim().max(100),
+    tipo_documento: z.string(),
+    numero_documento: z.string().max(100).trim(),
+    telefono: z.string().max(20).trim(),
+  }),
+});
+
+const CompanySchema = z.object({
+  userTypeData: z.object({
+    razon_social: z.string().trim().max(100),
+    tipo_documento_empresa: z.string(),
+    numero_documento_empresa: z.string().max(100).trim(),
+    telefono: z.string().max(20).trim(),
+    sitio_web: z.nullable(z.string().trim().max(100)),
+    camara_comercio: z.string(),
+    representante_legal: z.string().max(100).trim(),
+    tipo_documento_representante: z.string(),
+    numero_documento_representante: z.string().max(20).trim(),
+    correo_representante: z.string().trim().max(100).email().toLowerCase(),
+    descripcion: z.string(),
+  }),
+});
+
+const SignInSchema = z.object({
+  correo: z.string().trim().email().toLowerCase(),
+  contrasena: z.string().min(8).max(16),
+});
+
+const SignUpClientSchema = UserSchema.merge(ClientSchema);
+const SignUpCompanySchema = UserSchema.merge(CompanySchema);
+
+export const validateCreate = async (data, tipoUsuario) => {
+  if (tipoUsuario === 'cliente') {
+    return SignUpClientSchema.safeParseAsync(data);
+  }
+  if (tipoUsuario === 'empresa') {
+    return SignUpCompanySchema.safeParseAsync(data);
+  }
+  return UserSchema.safeParseAsync(data);
 };
 
-export const verifyPassword = (password, encryptPassword) => {
-  return compare(password, encryptPassword);
+export const validateSignIn = async (data) => {
+  return SignInSchema.safeParseAsync(data);
 };
+
+export const clientFields = [...Object.keys(SignUpClientSchema.shape)];
+export const companyFields = [...Object.keys(SignUpCompanySchema.shape)];
+export const userAdmin = [...Object.keys(UserSchema.shape)];
+export const signInFields = [...Object.keys(SignInSchema.shape)];
