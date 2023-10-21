5bd875c28939621433464fdfc291a54506e3a053
dianabedoya570 <40249472+dianabedoya570@users.noreply.github.com>
2023-10-21 10:42:18 -0500

    Pruebas de Orden de Productos (#53)
    * resolver conflictos
    
    * Update vitest.config.js

--- vitest.config.js
+++ vitest.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
   test: {
-    environment: 'node',
+    environment: "node",
     globals: true,
-    coverage: {
+     coverage: {
       provider: 'v8',
     },
   },
+  
 });
