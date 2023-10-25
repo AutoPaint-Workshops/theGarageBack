bde8336dec0c19157bb3f9e08b135270b804fef9
Welinton Suarez <126529767+wesgdev1@users.noreply.github.com>
2023-10-25 14:40:47 -0500

    resolve conflic 1 (#58)

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import http from "http";
+import { Server } from "socket.io";
 
 import { configuration } from "./app/config.js";
 import { app } from "./app/index.js";
@@ -9,6 +10,25 @@ const { port } = configuration.server;
 connect();
 
 const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`-->  user connected ${socket.id}`);
+
+  socket.on("disconnect", () => {
+    console.log(`-->  user disconnected ${socket.id}`);
+  });
+
+  socket.on("mensaje", (payload) => {
+    console.log(payload);
+    io.emit("mensaje", payload);
+  });
+});
 
 server.listen(port, () => {
   console.log(`Server running at ${port} port`);
